fb7fc1edf76b47bfd9ea1e340dae0f0a88dc3e40
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-01-13 21:16:19 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 import data from './data';
-import SingleQuestion from './Question';
+import Question from './Question';
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [quesions, setQuesions] = useState(data)
+  return <main>
+    <div className="container">
+      <h2>Accordion</h2>
+      <section className='info'>
+      {quesions.map((question)=>{
+        return< Question key={question.id}{...question} />
+      })}
+      </section>
+    </div>
+  </main>;
 }
 
 export default App;
